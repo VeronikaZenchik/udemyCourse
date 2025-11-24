ce44d800e9f1d94313e5c66704c06feefa78b0dc
VeronikaZenchik <veronikazenchik991@gmail.com>
2025-11-24 15:15:03 +0300

    dom challenge

--- JavaScript/The Document Object Model (DOM)/DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
+++ JavaScript/The Document Object Model (DOM)/DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
@@ -1,1 +1,4 @@
-document.query
+let thirdLi = document.querySelector("li:last-child").innerHTML = "Veronika"
+
+
+// document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Veronika";
