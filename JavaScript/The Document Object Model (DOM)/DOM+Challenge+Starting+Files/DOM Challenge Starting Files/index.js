86bae0b8680dc133284456ffc6db56e5c338fddf
VeronikaZenchik <veronikazenchik991@gmail.com>
2025-11-25 14:02:56 +0300

    doc practice

--- JavaScript/The Document Object Model (DOM)/DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
+++ JavaScript/The Document Object Model (DOM)/DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
@@ -1,4 +1,6 @@
 let thirdLi = document.querySelector("li:last-child").innerHTML = "Veronika"
-
+let linkLi = document.querySelector("li a").style.color = "green"
+let button = document.querySelector("button").style.backgroundColor = "yellow"
+let title = document.querySelector("h1").classList.add("huge")
 
 // document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Veronika";
