8feab6485b3652ce5b8c9f4777ca723b81c32eb5
VeronikaZenchik <veronikazenchik991@gmail.com>
2025-11-27 12:23:45 +0300

    finish project

--- JavaScript/The Dicee Challenge/index.js
+++ JavaScript/The Dicee Challenge/index.js
@@ -1,5 +1,6 @@
 let playerOne = document.querySelector(".img1")
 let playerTwo = document.querySelector(".img2")
+let title = document.querySelector("h1")
 let randomNumber1 = Math.floor(Math.random() * 6) + 1
 let randomNumber2 = Math.floor(Math.random() * 6) + 1
 console.log(randomNumber1, randomNumber2);
@@ -22,3 +23,32 @@ if (randomNumber1 === 5) {
 if (randomNumber1 === 6) {
   playerOne.setAttribute('src', './images/dice6.png')
 } 
+
+
+if (randomNumber2 === 1) {
+  playerTwo.setAttribute('src', './images/dice1.png')
+} 
+if (randomNumber2 === 2) {
+  playerTwo.setAttribute('src', './images/dice2.png')
+} 
+if (randomNumber2 === 3) {
+  playerTwo.setAttribute('src', './images/dice3.png')
+} 
+if (randomNumber2 === 4) {
+  playerTwo.setAttribute('src', './images/dice4.png')
+} 
+if (randomNumber2 === 5) {
+  playerTwo.setAttribute('src', './images/dice5.png')
+} 
+if (randomNumber2 === 6) {
+  playerTwo.setAttribute('src', './images/dice6.png')
+} 
+
+
+if(randomNumber1 > randomNumber2){
+  title.innerHTML ='<h1>Player One Wins!</h1>'
+} else if (randomNumber2 > randomNumber1) {
+  title.innerHTML ='<h1>Player Two Wins!</h1>'
+} else {
+  title.innerHTML ='<h1>Draw!</h1>'
+}
