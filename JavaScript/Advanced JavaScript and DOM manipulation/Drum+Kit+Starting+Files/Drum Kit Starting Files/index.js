6c299d573aedcf551e88f07be2b15df9b1c60c96
VeronikaZenchik <veronikazenchik991@gmail.com>
2025-12-01 15:58:59 +0300

    background images

--- JavaScript/Advanced JavaScript and DOM manipulation/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ JavaScript/Advanced JavaScript and DOM manipulation/Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -1,6 +1,9 @@
 for (let i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener('click', function(){
-    alert("I got clicked!")
+    let audio = new Audio ('sounds/tom-1.mp3')
+    audio.play()
   })
   
 }
+
+
