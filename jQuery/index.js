09ae4b87d8ef693a06020c736c2b8bdec6f941fc
VeronikaZenchik <veronikazenchik991@gmail.com>
2025-12-05 16:27:55 +0300

    event listenet

--- jQuery/index.js
+++ jQuery/index.js
@@ -2,3 +2,33 @@ $("h1").addClass("big-title margin-50")
 $("button").html("<em>Hey</em>")
 
 $("a").attr("href", "https://www.yahoo.com")
+
+$("h2").click(function(){
+  $("h2").css("color", "purple")
+})
+
+
+// for (let i = 0; i < 5; i++) {
+//   document.querySelectorAll("button")[i].addEventListener("click", function(){
+//     document.querySelector("h1").style.color = "purple"
+//   })
+// }
+
+
+$("button").click(function(){
+  $("h1").css("color", "red")
+})
+
+$("input").keydown(function(event){
+  console.log(event.key);
+  
+})
+
+$(document).keydown(function(event){
+  $('h1').text(event.key)
+})
+
+
+$("h1").on("mouseover", function(){
+  $("h1").css("color", "blue")
+})
