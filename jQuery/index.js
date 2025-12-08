bd5f38a8075270b870fed708437865d63cd9a4e5
VeronikaZenchik <veronikazenchik991@gmail.com>
2025-12-08 12:34:57 +0300

    addind and removing el

--- jQuery/index.js
+++ jQuery/index.js
@@ -32,3 +32,8 @@ $(document).keydown(function(event){
 $("h1").on("mouseover", function(){
   $("h1").css("color", "blue")
 })
+
+$("h1").before("<button>Before</button>")
+$("h1").after("<button>After</button>")
+$("h1").prepend("<button>Prepend</button>")
+$("h1").remove()
